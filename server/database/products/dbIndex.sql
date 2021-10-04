40843ad72761377733e7cb19e4a3caf6fbdf8b6e
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-04 13:46:01 -0400

    Add index to related table

--- server/database/products/dbIndex.sql
+++ server/database/products/dbIndex.sql
@@ -1,17 +1,17 @@
 CREATE INDEX idx_id
    ON products(id);
 
-
 CREATE INDEX idx_feature_productId
    ON features(product_id);
 
-
 CREATE INDEX idx_styles
    ON styles(id, productId);
-
 
 CREATE INDEX idx_skus_styleId
    ON skus(styleId);
 
 CREATE INDEX idx_photos_styleId
    ON photos(styleId);
+
+CREATE INDEX idx_related_productId
+   ON related(current_product_id);
