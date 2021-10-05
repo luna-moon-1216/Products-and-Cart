3399dd82861c2d494ab13908bbd123bb44768c69
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-05 18:13:36 -0400

    Add pool connect

--- server/database/products/index.js
+++ server/database/products/index.js
@@ -3,18 +3,32 @@ const { dbConfig } = require('./config.js');
 
 const pool = new Pool({
   user: 'ubuntu',
-  host: 'ec2-3-84-145-8.compute-1.amazonaws.com',
-  // max: 100,
+  host: '3.84.145.8',
   database: 'sdc_products',
   password: dbConfig.password,
   port: process.env.PORT || 5432
 });
 
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-  end: () => {
-    pool.end()
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack)
   }
-}
+  client.query('SELECT NOW()', (err, result) => {
+    release()
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    console.log(result.rows)
+  })
+})
+
+
+// module.exports = {
+//   query: (text, params, callback) => {
+//     return pool.query(text, params, callback)
+//   },
+//   end: () => {
+//     pool.end()
+//   }
+// }
+module.exports = pool;
