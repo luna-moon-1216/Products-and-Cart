0d5eb5c61c02650adf7541b4c6aa355f475f910a
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-05 18:54:17 -0400

    -

--- server/database/products/index.js
+++ server/database/products/index.js
@@ -3,32 +3,17 @@ const { dbConfig } = require('./config.js');
 
 const pool = new Pool({
   user: 'ubuntu',
-  host: '3.84.145.8',
+  host: 'ec2-3-84-145-8.compute-1.amazonaws.com',
   database: 'sdc_products',
   password: dbConfig.password,
-  port: process.env.PORT || 5432
+  port: 5432
 });
 
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack)
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  },
+  end: () => {
+    pool.end()
   }
-  client.query('SELECT NOW()', (err, result) => {
-    release()
-    if (err) {
-      return console.error('Error executing query', err.stack)
-    }
-    console.log(result.rows)
-  })
-})
-
-
-// module.exports = {
-//   query: (text, params, callback) => {
-//     return pool.query(text, params, callback)
-//   },
-//   end: () => {
-//     pool.end()
-//   }
-// }
-module.exports = pool;
+};
