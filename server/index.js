2f1d4d993d941feedddd2b9b9a35d5d4c2f55f1a
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-06 15:05:05 -0400

    Have to restart server

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ var router = require("./routes.js");
 
 app.use(express.json()); //need to put before the router middleware to make it work
 app.use("/catwalk", router);
-app.get("/loaderio-7a2836f21df8314d16d203266ea7df94", (req, res) => {
-  res.send("loaderio-7a2836f21df8314d16d203266ea7df94");
+app.get("/loaderio-d696ff881d540cdfc3b3a04350397744", (req, res) => {
+  res.send("loaderio-d696ff881d540cdfc3b3a04350397744");
 });
 
 app.use(express.static(__dirname + "/../dist"));
