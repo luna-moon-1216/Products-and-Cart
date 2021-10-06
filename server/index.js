e06488afff9836e2c7c4f0e7d54f4456451b7bbb
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-06 19:39:05 -0400

    Restart AWS

--- server/index.js
+++ server/index.js
@@ -8,15 +8,10 @@ var router = require("./routes.js");
 
 app.use(express.json()); //need to put before the router middleware to make it work
 app.use("/catwalk", router);
-app.get("/loaderio-05425e451bc782628a76696ee8e789d9.html", (req, res) => {
-  res.send("loaderio-05425e451bc782628a76696ee8e789d9");
+app.get("/loaderio-a182a72bf27e0e7fa96c3c2734a4b213", (req, res) => {
+  res.send("loaderio-a182a72bf27e0e7fa96c3c2734a4b213");
 });
-app.get("/loaderio-05425e451bc782628a76696ee8e789d9.txt", (req, res) => {
-  res.send("loaderio-05425e451bc782628a76696ee8e789d9");
-});
-app.get("/loaderio-05425e451bc782628a76696ee8e789d9/", (req, res) => {
-  res.send("loaderio-05425e451bc782628a76696ee8e789d9");
-});
+
 
 
 app.use(express.static(__dirname + "/../dist"));
