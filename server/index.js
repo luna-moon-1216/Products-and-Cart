dda5e00d3b96f3a14726d1ac8c89b5b644ad5e19
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-06 11:22:58 -0400

    Add loader io token

--- server/index.js
+++ server/index.js
@@ -4,16 +4,16 @@ const morgan = require("morgan");
 
 let app = express();
 
-var router = require('./routes.js');
+var router = require("./routes.js");
 
 app.use(express.json()); //need to put before the router middleware to make it work
-app.use('/catwalk', router);
+app.use("/catwalk", router);
+app.get("/loaderio-7a2836f21df8314d16d203266ea7df94", (req, res) => {
+  res.send("loaderio-7a2836f21df8314d16d203266ea7df94");
+});
 
 app.use(express.static(__dirname + "/../dist"));
 
 app.use(morgan("dev"));
 
 module.exports = app;
-
-
-
