3ce4fb9fc329cee83d0f81a3bc1c014b71348be2
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-06 19:55:06 -0400

    - -

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ var router = require("./routes.js");
 
 app.use(express.json()); //need to put before the router middleware to make it work
 app.use("/catwalk", router);
+app.get("/loaderio-a182a72bf27e0e7fa96c3c2734a4b213.html", (req, res) => {
+  res.send("loaderio-a182a72bf27e0e7fa96c3c2734a4b213");
+});
+app.get("/loaderio-a182a72bf27e0e7fa96c3c2734a4b213.txt", (req, res) => {
+  res.send("loaderio-a182a72bf27e0e7fa96c3c2734a4b213");
+});
 app.get("/loaderio-a182a72bf27e0e7fa96c3c2734a4b213", (req, res) => {
   res.send("loaderio-a182a72bf27e0e7fa96c3c2734a4b213");
 });
