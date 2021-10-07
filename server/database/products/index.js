99f7a4a92f160bc27a2279b564a8ffd91b6931f4
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-06 22:29:20 -0400

    -

--- server/database/products/index.js
+++ server/database/products/index.js
@@ -10,27 +10,27 @@ const pool = new Pool({
   port: 5432
 });
 
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-  end: () => {
-    pool.end()
-  }
-};
-
-// pool.connect((err, client, done) => {
-//   if (err) {
-//     return console.error("Error acquiring client", err.stack);
+// module.exports = {
+//   query: (text, params, callback) => {
+//     return pool.query(text, params, callback)
+//   },
+//   end: () => {
+//     pool.end()
 //   }
-//   client.query("SELECT NOW()", (err, result) => {
-//     done();
-//     if (err) {
-//       return console.error("Error executing query", err.stack);
-//     } else {
-//     console.log("CONNECTED: ", result.rows);
-//     }
-//   });
-// });
+// };
 
-// module.exports = pool;
+pool.connect((err, client, done) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack);
+  }
+  client.query("SELECT NOW()", (err, result) => {
+    done();
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    } else {
+    console.log("CONNECTED: ", result.rows);
+    }
+  });
+});
+
+module.exports = pool;
